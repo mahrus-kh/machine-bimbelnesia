ec871aec53073c097bb7c2b4d8662766a5f868ef
mahrus-kh <mahrus.developer@gmail.com>
2018-03-06 19:43:16 +0700

    TF-IDF is done, jadi array 2 dimensi, tinggal penyempurnaan, cara dengan fungsi terpisah

--- TF-IDF/TFIDFClass.php
+++ TF-IDF/TFIDFClass.php
@@ -12,7 +12,7 @@ class TFIDFClass
         return $koneksi;
     }
 
-    public function lbb() // mengambil lbb_id yang ada di tb_lbb => setiap lbb
+    public function lbb() // mengambil id lbb yang ada di tb_lbb => setiap lbb
     {
         $tampil = $this->koneksi()->prepare("SELECT id FROM tb_lbb");
         $tampil->execute();
@@ -25,56 +25,7 @@ class TFIDFClass
         return $lbb;
     }
 
-    public function term_lbb() //mengambil term setiap lbb
-    {
-        $lbb_id = $this->lbb();
-        for ($i=0;$i<count($lbb_id);$i++){
-            $ambil = $this->koneksi()->prepare("SELECT term FROM tb_term WHERE lbb_id=:lbb_id");
-            $ambil->bindParam(":lbb_id", $lbb_id[$i]);
-            $ambil->execute();
-            $ambil->setFetchMode(PDO::FETCH_ASSOC);
-            if (count($ambil)>0){
-                while ($data=$ambil->fetch(PDO::FETCH_ORI_NEXT)){
-                    $term_lbb [$lbb_id[$i]] =  explode(",", $data['term']);
-                }
-            }
-        }
-        return $term_lbb;
-    }
-
-    public function term_aktivitas() //contoh sementara > selesaikan
-    {
-        $term = "bimbel,sd,smp,sma,stan,pns,forum,guru,malang,ujian,nasional"; //contoh dan harus bersih -> dari database -> sementara
-        $term = explode(",", $term);
-
-        return $term;
-    }
-
-    public function tabel_term_lbb()
-    {
-        $term = $this->term_aktivitas();
-        $lbb_id = $this->lbb();
-    }
-
-    public function coba_in() //langsung hitung tf dalam satu waktu
-    {
-        $term_lbb = $this->term_lbb();
-        $term = ["bimbel","dua","tujuh","tiga"];
-        $count = 0 ;
-
-        for ($i=0;$i<count($term);$i++){ // i adalah term
-            for ($j=0;$j<=count($term_lbb);$j++){ // j adalah total lbb dimulai dari id ada
-                for ($k=0;$k<count($term_lbb[$j]);$k++){
-                    if ($term[$i] == $term_lbb[$j][$k]) {
-                        $count = $count + 1;
-                    }
-                }
-            }
-        }
-        return $count ;
-    }
-
-    public function coba_nah() //hitung tf dengan fungsu tf terpisah
+    public function coba_nah() //hitung tf dengan fungsu tf terpisah //percobaan logika
     {
         $lbb_id = $this->lbb();
         $term_aktivitas = ["bimbel","dua","tujuh","tiga"];
@@ -84,11 +35,10 @@ class TFIDFClass
                 $return [$term_aktivitas[$i]] [$lbb_id[$j]] = $this->hitung_tf($term_aktivitas[$i],$lbb_id[$j]);
             }
         }
-
         return $return;
     }
 
-    public function hitung_tf($term_aktivitas, $lbb_id)
+    public function hitung_tf($term_aktivitas, $lbb_id) // hitung tf dari term_aktivitas terhadapt term di lbb_id
     {
         $ambil = $this->koneksi()->prepare("SELECT term FROM tb_term WHERE lbb_id=:lbb_id");
         $ambil->bindParam(":lbb_id", $lbb_id);
@@ -100,16 +50,68 @@ class TFIDFClass
             }
         }
 
-        $total = 0;
+        $hitung_tf= 0;
         for ($i=0;$i<count($term_lbb);$i++){ // cek berapa jumlah term_aktivitas_t yang ada di term_Lbb
             if ($term_aktivitas == $term_lbb[$i]){
-                $total = $total + 1;
+                $hitung_tf = $hitung_tf + 1;
             }
         }
-        return $total;
+        return $hitung_tf;
     }
 
+    public function tabel_tf() //membuat tabel tf setiap term_aktivitas terhadap term setiap lbb
+    {
+        $lbb_id = $this->lbb();
+        $term_aktivitas = ["bimbel","malang","ujian","lulus"];
 
+        for ($i=0;$i<count($term_aktivitas);$i++){
+            for ($j=0;$j<count($lbb_id);$j++){
+                $tabel_tf [$term_aktivitas[$i]] [$lbb_id[$j]] = $this->hitung_tf($term_aktivitas[$i],$lbb_id[$j]);
+            }
+        }
+        return $tabel_tf;
+    }
+
+    public function tabel_df() //menghitung df setiap term_aktivitas
+    {
+        $tabel_tf = $this->tabel_tf();
+//
+        foreach ($tabel_tf as $term => $row ){ //$term = term_aktivitas, $row = array nilai tf
+            $hitung_df = 0;
+            foreach ($row as $tf) {
+                if ($tf > 0)
+                $hitung_df = $hitung_df + 1;
+            }
+            $tabel_df [$term] = $hitung_df;
+        }
+
+    return $tabel_df;
+    }
+
+    public function tabel_idf()
+    {
+        $tabel_df = $this->tabel_df();
+        foreach ($tabel_df as $term => $df) { //$term = term_aktivitas, $df = nilai df setiap term_aktivitas
+            $tabel_idf [$term] = 1 / $df;
+        }
+
+        return $tabel_idf;
+    }
+    public function tabel_tf_idf()
+    {
+        $tabel_tf = $this->tabel_tf();
+        $tabel_idf = $this->tabel_idf();
+
+        foreach ($tabel_tf as $key => $row){ //$key = term_aktvitas, $row = array setiap term_aktvitas (tf setiap term_aktivitas terhadap lbb)
+            foreach ($row as $id_lbb => $tf) { //$id_lbb = id_lbb yang ada tf nya, $tf = nilai tf setiap $lbb
+                $tabel_tf_idf [$key] [$id_lbb] = $tf * $tabel_idf[$key];
+            }
+        }
+
+        return $tabel_tf_idf;
+    }
+
+    //tambahan hapus tabel
     public function truncate_tb_term()
     {
         $del = $this->koneksi()->prepare("TRUNCATE TABLE tb_term");
--- TF-IDF/TFIDFProses.php
+++ TF-IDF/TFIDFProses.php
@@ -3,9 +3,16 @@
 include "TFIDFClass.php";
 
 $TFIDFProses = New TFIDFClass();
+$awal = microtime(true);
 
 //var_dump($TFIDFProses->tabel_term_lbb());
 
 //var_dump($TFIDFProses->term_lbb());
 
-var_dump($TFIDFProses->coba_nah());
+var_dump($TFIDFProses->tabel_tf_idf());
+
+//$table_tf = $TFIDFProses->tabel_tf();
+//
+//foreach ($table_tf as $row => $r){
+//    echo $r . " : ";
+//}
